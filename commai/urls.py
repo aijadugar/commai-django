478a85ce2f56514ce6c02215e57f0d51cb53ca5a
BARI ANKIT VINOD <139578960+OnlyCR7@users.noreply.github.com>
2025-01-25 21:46:25 +0530

    all feats are intergrated

--- commai/urls.py
+++ commai/urls.py
@@ -24,6 +24,8 @@ urlpatterns = [
     path('services/text_interaction/', views.text_interaction, name='text_interaction'),
     path('services/speech_interaction/', views.speech_interaction, name = 'speech_interaction'),
     path('services/daily_tasks/', views.daily_tasks, name = 'daily_tasks'),
+    path('submit_writing_exercise/', views.submit_writing_exercise, name='submit_writing_exercise'),
+    path('submit_speaking_exercise/', views.submit_speaking_exercise, name='submit_speaking_exercise'),
     path('partners/partners/', views.partners, name = 'partners'),
     path('contactus/contactus/', views.contactus, name = 'contactus'),
 ]
