8e34b586b5fc11383a017ddd2289305ea55faf36
Yash Kerkar <149948089+YashKerkarTech04@users.noreply.github.com>
2025-06-30 18:29:08 +0530

    Update ztest.py

--- ztest.py
+++ ztest.py
@@ -5,7 +5,7 @@ import google.generativeai as genai
 
 model = genai.GenerativeModel('gemini-1.5-pro-latest')
 
-G_API_KEY = 'AIzaSyC1l9rED1nJeliRvS3LtWD3IxfC_Goue0E'
+G_API_KEY = 'api-key-here'
 
 genai.configure(api_key = G_API_KEY)
 
